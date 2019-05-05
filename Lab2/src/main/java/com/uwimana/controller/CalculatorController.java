95af60eb2e2011b3cc31ac50ada2f56eea7b9251
chrisuwimana1 <chrisuwimana1@gmail.com>
2019-05-05 15:10:19 -0500

    Lab 2

---
+++ Lab2/src/main/java/com/uwimana/controller/CalculatorController.java
@@ -0,0 +1,43 @@
+package com.uwimana.controller;
+
+import com.uwimana.domain.Calculator;
+import com.uwimana.service.CalculatorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+//Lab3/CalculatorLab3/src/main/webapp/WEB-INF/views/calculator.jsp
+@Controller
+public class CalculatorController {
+
+    @Autowired
+    private CalculatorService calculatorService;
+
+//    public void setCalculatorService(CalculatorService calculatorService) {
+//        this.calculatorService = calculatorService;
+//    }
+
+    @RequestMapping(value = "/calculator", method = RequestMethod.GET)
+    public String showForm(Calculator calculator) throws Exception{
+        //return new ModelAndView("calculator", "calculator", new Calculator());
+        return "calculator";
+    }
+
+    @RequestMapping(value = "/calculator", method = RequestMethod.POST)
+    public String calculate(Calculator calculator) throws Exception{
+
+//        model.addAttribute("num1", calculator.getNum1());
+//        model.addAttribute("num2", calculator.getNum2());
+//        model.addAttribute("num3", calculator.getNum3());
+//        model.addAttribute("num4", calculator.getNum4());
+//
+//        model.addAttribute("sum", calculatorService.getSum());
+//        model.addAttribute("product", calculatorService.getProduct());
+        calculatorService.add(calculator);
+        calculatorService.mult(calculator);
+
+        return "result";
+    }
+
+}
---
+++ Lab2/src/main/java/com/uwimana/service/CalculatorService.java
@@ -0,0 +1,11 @@
+package com.uwimana.service;
+
+import com.uwimana.domain.Calculator;
+
+public interface CalculatorService {
+
+    public void add(Calculator calculator);
+    public void mult(Calculator calculator);
+
+
+}
